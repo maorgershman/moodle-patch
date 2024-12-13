099a4c6da14cd44e374dc1da903e7f395aa7fc72
maorgershman <maorgershman@campus.technion.ac.il>
2024-12-13 03:14:45 +0200

    Add prefetch for links and resources

--- patch-links.js
+++ patch-links.js
@@ -1,11 +1,74 @@
+const fetchActualUrl = async (href) => {
+  const res = await fetch(href);
+  let html = await res.text();
+
+  // Search resource div
+  let delim = `workaround">`;
+  let index = html.indexOf(delim);
+  if (index == -1) {
+    return null;
+  }
+  index += delim.length;
+
+  html = html.substring(index);
+  delim = `</div>`;
+  index = html.indexOf(delim);
+  if (index == -1) {
+    return null;
+  }
+  html = html.substring(0, index);
+
+  // Search link
+  delim = `<a href="`;
+  index = html.indexOf(delim);
+  if (index == -1) {
+    return null;
+  }
+  index += delim.length;
+
+  html = html.substring(index);
+  delim = '"';
+  index = html.indexOf(delim);
+  html = html.substring(0, index);
+  
+  return html;
+};
+
+const lazy = false;
+
 window.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("a").forEach((a) => {
+  document.querySelectorAll("a").forEach(async (a) => {
     if (a.hasAttribute("target") || !a.hasAttribute("onclick")) {
       return;
     }
 
     a.setAttribute("target", "_blank");
     a.removeAttribute("onclick");
-    a.parentNode.replaceChild(a.cloneNode(true), a);
+
+    const clone = a.cloneNode(true);
+    if (a.classList.contains("aalink")) {
+      if (lazy) {
+        const eventListener = async (event) => {
+          event.preventDefault();
+
+          let url = await fetchActualUrl(a.href);
+          if (url == null) {
+            clone.removeEventListener("click", eventListener);
+            url = a.href;
+          }
+
+          window.open(url, "_blank");
+        };
+
+        clone.addEventListener("click", eventListener);
+      } else {
+        const url = await fetchActualUrl(a.href);
+        if (url != null) {
+          clone.setAttribute("href", url);
+        }
+      }
+    }
+
+    a.parentNode.replaceChild(clone, a);
   });
 });
